a3bb9a282d1ab1f3817a526f5d94ea58e33652c9
Ahmen Batuhan KIR <79552743+kule1163@users.noreply.github.com>
2022-09-11 00:55:26 +0300

    make it better

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const CONNETION_URL = process.env.MONGODB_URI;
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 interface OnlineUser {
   id: string;
